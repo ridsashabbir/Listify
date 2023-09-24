588da58e63d59cfd7ba8ebdccfb07cdd56b2a489
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-09-25 00:46:27 +0500

    added styling for login page

--- listify/src/Components/Login.js
+++ listify/src/Components/Login.js
@@ -13,32 +13,34 @@ function Login() {
 
   return (
     <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+      <div className="login-form">
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        <div className="link">
+          Don't have an account? <Link to="/signup">Signup</Link>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-      <div className="link">
-        Don't have an account? <Link to="/signup">Signup</Link>
       </div>
     </div>
   );
