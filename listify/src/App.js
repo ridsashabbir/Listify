b8bd07c4542aa7bd6987498fda0aaefe5a686ce0
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-09-24 22:49:19 +0500

    login screen

--- listify/src/App.js
+++ listify/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Signup from "./Components/Signup";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <>
-      <Signup />
+      {/* <Signup /> */}
+      <Login />
     </>
   );
 }
